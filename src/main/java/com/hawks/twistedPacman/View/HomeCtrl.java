2643dba6dcd98b4c4216d364d8d05e66de1e9538
samark21 <samar.kardosh@gmail.com>
2021-11-15 20:55:24 +0200

    Commit2 - screens shield is done. missing buttons functionalities and fx:id.

--- src/main/java/com/hawks/twistedPacman/View/HomeCtrl.java
+++ src/main/java/com/hawks/twistedPacman/View/HomeCtrl.java
@@ -1,15 +1,57 @@
 package com.hawks.twistedPacman.View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeCtrl {
 
-//    Button btn = (Button)findViewById(R.id.open_activity_button);
-//
-//btn.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            startActivity(new Intent(MainActivity.this, MyOtherActivity.class));
-//        }
-//    });
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    @FXML
+    private Button playBtn;
+    private Button adminBtn;
+    private Button exitBtn;
+    private Button instructionsBtn;
+    private Button seeMoreBtn;
+
+    @FXML
+    private void playClicked(ActionEvent event) throws IOException {
+//        root = FXMLLoader.load(getClass().getResource("nickname.fxml"));
+//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+    }
+
+    @FXML
+    private void adminClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void exitClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void instructionsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void seeMoreClicked(ActionEvent event) {
+
+    }
 }
+
