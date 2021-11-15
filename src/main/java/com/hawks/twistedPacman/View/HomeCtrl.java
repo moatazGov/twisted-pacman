21897e275471c376f2045b2b3838459d3cdd9c44
samark21 <samar.kardosh@gmail.com>
2021-11-15 13:57:22 +0200

    Commit1 - screens shield is done. missing buttons functionalities.

---
+++ src/main/java/com/hawks/twistedPacman/View/HomeCtrl.java
@@ -0,0 +1,15 @@
+package com.hawks.twistedPacman.View;
+
+import javafx.scene.control.Button;
+
+public class HomeCtrl {
+
+//    Button btn = (Button)findViewById(R.id.open_activity_button);
+//
+//btn.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            startActivity(new Intent(MainActivity.this, MyOtherActivity.class));
+//        }
+//    });
+}
